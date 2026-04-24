6b79fd77cf029516b7f8033fd63f906e7b8990ea
Brent Anderson <brent@knock.app>
2026-04-24 14:11:13 -0500

    fix

--- lib/premailex/html_parser/meeseeks.ex
+++ lib/premailex/html_parser/meeseeks.ex
@@ -65,48 +65,4 @@ if Code.ensure_loaded?(Meeseeks) do
       |> Meeseeks.tree()
     end
   end
-
-  @impl true
-  @doc false
-  def all(tree, selector) do
-    selector = CSS.compile_selectors(selector)
-
-    tree
-    |> Meeseeks.parse(:tuple_tree)
-    |> Meeseeks.all(selector)
-    |> Enum.map(&Meeseeks.tree/1)
-  rescue
-    e in Meeseeks.Error ->
-      Logger.debug("Meeseeks error: " <> inspect(e))
-      []
-  end
-
-  @impl true
-  @doc false
-  def to_string(tree) do
-    tree
-    |> Meeseeks.parse(:tuple_tree)
-    |> Meeseeks.html()
-  end
-
-  @impl true
-  @doc false
-  def text(text) when is_binary(text), do: text
-  def text(list) when is_list(list), do: Enum.map_join(list, "", &text/1)
-  def text({:comment, _text}), do: ""
-  def text({_element, _attrs, children}), do: text(children)
-
-  @impl true
-  @doc false
-  def filter(tree, selector) do
-    selector = CSS.compile_selectors(selector)
-    tree = Meeseeks.parse(tree, :tuple_tree)
-
-    tree
-    |> Meeseeks.all(selector)
-    |> Enum.reduce(tree, fn e, acc ->
-      Document.delete_node(acc, e.id)
-    end)
-    |> Meeseeks.tree()
-  end
 end
